8a739d156f9264e0ffdc2cbf3e248f774fefd032
Alejandro Vilar <alevilar@gmail.com>
2018-10-03 22:13:24 -0300

    Update WkHtmlToPdfEngine.php

--- Pdf/Engine/WkHtmlToPdfEngine.php
+++ Pdf/Engine/WkHtmlToPdfEngine.php
@@ -26,7 +26,8 @@ class WkHtmlToPdfEngine extends AbstractPdfEngine {
  * @return string raw pdf data
  */
 	public function output() {
-		$content = $this->_exec($this->_getCommand(), $this->_Pdf->html());
+		// $content = $this->_exec($this->_getCommand(), $this->_Pdf->html());
+		$content = $this->_exec('xvfb-run -a -s "-screen 0 640x480x16" ' . $this->_getCommand(), $this->_Pdf->html());
 
 		if (strpos(mb_strtolower($content['stderr']), 'error')) {
 			throw new CakeException("System error <pre>" . $content['stderr'] . "</pre>");
